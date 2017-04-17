e9cd3289e6ac4ecf0258ce48be5f8b5cba285d15
RCdiy <info@RCdiy.ca>
2017-04-17 15:02:14 -0400

    Published new

---
+++ Mac Apps Scripts/GenerateAllPacks.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+# License https://www.gnu.org/licenses/gpl-3.0.en.html
+# Mac OpenTX Sound Pack Generator
+# SOUNDS
+# Place this file in the root of the SD Card copy on your computer
+#
+# Author: RC diy
+# Web: http://RCdiy.ca
+version="2.2V0009"
+sdcard="sdcard-horus-"$version
+unzip -q $sdcard.zip -d temp
+pack=sounds-`echo $sdcard | cut -d - -f 2,3`
+mkdir $pack
+mv temp/SOUNDS $pack/SOUNDS
+cp temp/opentx.sdcard.version $pack/
+rm -rf temp
+cd $pack
+cp */*/*/*.csv .
+rm -rf SOUNDS
+../SoundPackGenerator.sh de-DE-taranis.csv
+../SoundPackGenerator.sh en-US-taranis.csv
+../SoundPackGenerator.sh es-ES-taranis.csv
+../SoundPackGenerator.sh fr-FR-taranis.csv
+../SoundPackGenerator.sh it-IT-taranis.csv
+cd ..
+
+sdcard="sdcard-taranis-x7-"$version
+unzip -q $sdcard.zip -d temp
+pack=sounds-`echo $sdcard | cut -d - -f 2,3`
+mkdir $pack
+mv temp/SOUNDS $pack/SOUNDS
+cp temp/opentx.sdcard.version $pack/
+rm -rf temp
+cd $pack
+cp */*/*/*.csv .
+rm -rf SOUNDS
+../SoundPackGenerator.sh de-DE-taranis.csv
+../SoundPackGenerator.sh en-US-taranis.csv
+../SoundPackGenerator.sh es-ES-taranis.csv
+../SoundPackGenerator.sh fr-FR-taranis.csv
+../SoundPackGenerator.sh it-IT-taranis.csv
+cd ..
+
+sdcard="sdcard-taranis-x9-"$version
+unzip -q $sdcard.zip -d temp
+pack=sounds-`echo $sdcard | cut -d - -f 2,3`
+mkdir $pack
+mv temp/SOUNDS $pack/SOUNDS
+cp temp/opentx.sdcard.version $pack/
+rm -rf temp
+cd $pack
+cp */*/*/*.csv .
+rm -rf SOUNDS
+../SoundPackGenerator.sh de-DE-taranis.csv
+../SoundPackGenerator.sh en-US-taranis.csv
+../SoundPackGenerator.sh es-ES-taranis.csv
+../SoundPackGenerator.sh fr-FR-taranis.csv
+../SoundPackGenerator.sh it-IT-taranis.csv
+cd ..
